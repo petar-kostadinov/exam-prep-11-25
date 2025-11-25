04fa58d157a57b0a8244b1633db336dc6fec304f
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-11-25 20:48:00 +0200

    change views directory

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ app.engine('hbs', handlebars.engine({
 
 app.set('view engine', 'hbs');
 
+app.set('views', 'src/views');
+
 app.use(routes);
 
 app.listen(3000, () => console.log('Server is listening on http://localhost:3000....'));
